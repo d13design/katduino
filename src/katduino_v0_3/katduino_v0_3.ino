cada44bc16e135c42ff99c0e55631e45ee95e22e
Dave Waller <davewallr@gmail.com>
2019-05-20 10:19:20 +0100

    Version 0.3 - first fully working version :smile:

--- src/katduino_v0_3/katduino_v0_3.ino
+++ src/katduino_v0_3/katduino_v0_3.ino
@@ -20,7 +20,7 @@ unsigned long timeToWaitForMomentary = 600; // time (in ms) to hold these button
 
 // Define app name and version
 const String APP_NAME = "KATDUINO";
-const String APP_VERSION = "0.2";
+const String APP_VERSION = "0.3";
 
 // Define footswitch pins
 #define S1 10
@@ -212,14 +212,6 @@ void setup() {
   }else{
     s4Mom = false;
   }
-
-  // REMOVE THIS WHEN PROGRAMMING MODE IS DONE
-  s1Mom = false;
-  s2Mom = false;
-  s3Mom = true;
-  s4Mom = false;
-  currMom = 3;
-  // END
   
   // Update memory settings just in case they weren't set before
   EEPROM.write(S_ADDR, currState);
@@ -243,105 +235,129 @@ void loop() {
   bool s3Pressed = digitalRead(S3) == H;
   bool s4Pressed = digitalRead(S4) == H;
   pressed = stringPresses(s1Pressed, s2Pressed, s3Pressed, s4Pressed);
-
-  // Are we in programming mode or not?
-  if(!inProgrammingMode){ // Not in programming mode
     
-    // If the switch has changed, reset the timer
-    if(pressed != lastPressed){
-      timeSinceLastPress = millis();
-      if(pressed == enterProgramModeCode){
-        // The "enter momentary programming mode" switches are being pressed
-        askedForMomentary = millis();
-      }else{
-        askedForMomentary = 0;
-      }
+  // If the switch has changed, reset the timer
+  if(pressed != lastPressed){
+    timeSinceLastPress = millis();
+    if(pressed == enterProgramModeCode || pressed == exitProgramModeCode){
+      // The "enter momentary programming mode" switches are being pressed
+      askedForMomentary = millis();
+    }else{
+      askedForMomentary = 0;
     }
+  }
 
-    // Check to see if we should enter momentary programming mode or not
-    if(askedForMomentary == 0){ // Business as usual
-      // We want to have had the buttons in the same state for {debounceTime} milliseconds before counting it as an actual press
-      if((millis() - timeSinceLastPress) > debounceTime){
-        // Yay! We should count this as an actual button press!
-        if(pressed == "0000" && momentaryOn){
-          // User has released all buttons and is in momentary mode - time to reset
-          momentaryOn = false;
-          setChannel(stateBeforeMom);
-        }else{
-          if(countSwitches(s1Pressed, s2Pressed, s3Pressed, s4Pressed)==1){ // Only one button is being pressed
-            // Work out which button has been pressed
-            int whichButton;
-            if(s1Pressed) whichButton = 1;
-            if(s2Pressed) whichButton = 2;
-            if(s3Pressed) whichButton = 3;
-            if(s4Pressed) whichButton = 4;
-            // If button is different to current state, switch channels
-            if(whichButton != currState){
-              // If a momentary button is set, and is being pressed we should switch on momentary mode
-              if(whichButton == currMom){
-                stateBeforeMom = currState;
-                momentaryOn = true;
-              }
-              setChannel(whichButton);
-            }
-          }
-        }
-      }
-    }else{ // We're being timed for entering momentary setting mode
-      if((millis() - askedForMomentary) > timeToWaitForMomentary){
-        // We've held the "enter programming mode" buttons down for long enough
-        inProgrammingMode = true;
-        lcd.clear();
-        lcd.print("PROGRAMMING MODE");
-        lcd.setCursor(0,1);
-        lcd.print("SET MOMENTARY: ");
-        lcd.blink();
-      }
-    }
-    
-  }else{ // We are in programming mode
-    
+  // Check to see if we should enter momentary programming mode or not
+  if(askedForMomentary == 0 && !inProgrammingMode){ // Not waiting for the momentary time to elapse / Not already in program mode -- Business as usual
     // We want to have had the buttons in the same state for {debounceTime} milliseconds before counting it as an actual press
     if((millis() - timeSinceLastPress) > debounceTime){
       // Yay! We should count this as an actual button press!
-      if(countSwitches(s1Pressed, s2Pressed, s3Pressed, s4Pressed)==1){ // Only one button is being pressed
-        // Work out which button has been pressed
-        int whichButton;
-        if(s1Pressed) whichButton = 1;
-        if(s2Pressed) whichButton = 2;
-        if(s3Pressed) whichButton = 3;
-        if(s4Pressed) whichButton = 4;
-        // If the momentary channel has changed...
-        if(currMom != whichButton){
-          // Update the current momentary
-          currMom = whichButton;
-          // Store the current momentary
-          s1Mom = false;
-          s2Mom = false;
-          s3Mom = false;
-          s4Mom = false;
-          if(currMom == 1) s1Mom = true;
-          if(currMom == 2) s2Mom = true;
-          if(currMom == 3) s3Mom = true;
-          if(currMom == 4) s4Mom = true;
-          EEPROM.write(S1_MOM_ADDR, s1Mom);
-          EEPROM.write(S2_MOM_ADDR, s2Mom);
-          EEPROM.write(S3_MOM_ADDR, s3Mom);
-          EEPROM.write(S4_MOM_ADDR, s4Mom);
-          // Update the programming mode display
-          lcd.setCursor(15,1);
-          lcd.print(currMom);
-          delay(1500);
-          // Exit programming mode
-          inProgrammingMode = false;
+      if(pressed == "0000" && momentaryOn){
+        // User has released all buttons and is in momentary mode - time to reset
+        momentaryOn = false;
+        setChannel(stateBeforeMom);
+      }else{
+        if(countSwitches(s1Pressed, s2Pressed, s3Pressed, s4Pressed)==1){ // Only one button is being pressed
+          // Work out which button has been pressed
+          int whichButton;
+          if(s1Pressed) whichButton = 1;
+          if(s2Pressed) whichButton = 2;
+          if(s3Pressed) whichButton = 3;
+          if(s4Pressed) whichButton = 4;
+          // If button is different to current state, switch channels
+          if(whichButton != currState){
+            // If a momentary button is set, and is being pressed we should switch on momentary mode
+            if(whichButton == currMom){
+              stateBeforeMom = currState;
+              momentaryOn = true;
+            }
+            setChannel(whichButton);
+          }
+        }
+      }
+    }
+  }
+
+  // In program mode but not pressing the exit buttons
+  if(askedForMomentary == 0 && inProgrammingMode){
+    // We're trying to set a button to act as a momentary switch
+    if(countSwitches(s1Pressed, s2Pressed, s3Pressed, s4Pressed)==1){ // Only one button is being pressed
+      // Work out which button has been pressed
+      int whichButton;
+      if(s1Pressed) whichButton = 1;
+      if(s2Pressed) whichButton = 2;
+      if(s3Pressed) whichButton = 3;
+      if(s4Pressed) whichButton = 4;
+      // If the momentary channel has changed...
+      if(currMom != whichButton){
+        // Update the current momentary
+        currMom = whichButton;
+        // Store the current momentary
+        s1Mom = false;
+        s2Mom = false;
+        s3Mom = false;
+        s4Mom = false;
+        if(currMom == 1) s1Mom = true;
+        if(currMom == 2) s2Mom = true;
+        if(currMom == 3) s3Mom = true;
+        if(currMom == 4) s4Mom = true;
+        EEPROM.write(S1_MOM_ADDR, s1Mom);
+        EEPROM.write(S2_MOM_ADDR, s2Mom);
+        EEPROM.write(S3_MOM_ADDR, s3Mom);
+        EEPROM.write(S4_MOM_ADDR, s4Mom);
+        // Update the programming mode display
+        lcd.setCursor(15,1);
+        lcd.print(currMom);
+        delay(1500);
+        // Exit programming mode
+        inProgrammingMode = false;
+        // Just in case we've set the momentary to the current channel
+        if(currMom == currState){
+          if(currMom == 1) setChannel(2);
+          if(currMom != 1) setChannel(1);
+        }else{
           // Reset the display
           setDisplay();
         }
       }
     }
-
-    
   }
+  
+  if(askedForMomentary != 0){ // We're being timed for entering or exiting momentary setting mode
+    if((millis() - askedForMomentary) > timeToWaitForMomentary && !inProgrammingMode && pressed==enterProgramModeCode){ // We want to enter programming mode and we're not already in it
+      // We've held the "enter programming mode" or "exit programming mode" buttons down for long enough
+      inProgrammingMode = true;
+      askedForMomentary = 0;
+      lcd.clear();
+      lcd.print("PROGRAMMING MODE");
+      lcd.setCursor(0,1);
+      lcd.print("SET MOMENTARY: ");
+      lcd.blink();
+      delay(1500);
+    }
+    if((millis() - askedForMomentary) > timeToWaitForMomentary && inProgrammingMode && pressed==exitProgramModeCode){ // We want to exit programming mode and we're already in it
+      // RESET MOMENTARY STUFF
+      s1Mom = false;
+      s2Mom = false;
+      s3Mom = false;
+      s4Mom = false;
+      currMom = 0;
+      EEPROM.write(S1_MOM_ADDR, s1Mom);
+      EEPROM.write(S2_MOM_ADDR, s2Mom);
+      EEPROM.write(S3_MOM_ADDR, s3Mom);
+      EEPROM.write(S4_MOM_ADDR, s4Mom);
+      // Update the programming mode display
+      lcd.setCursor(0,1);
+      lcd.print(" MOMENTARY OFF! ");
+      delay(1500);
+      // Exit programming mode
+      inProgrammingMode = false;
+      // Reset the display
+      setDisplay();
+    }
+  }
+
+ 
 
   lastPressed = pressed;
 }
